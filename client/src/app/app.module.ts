c00c932907cdcb7954c68844a40a85b2e768b8e9
gaborszekely <gabor_szekely@hotmail.com>
2019-06-08 18:25:22 -0600

    Updated ngrx to work with Redux dev tools extension and to track router state as well

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,8 +5,10 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ClarityModule } from "@clr/angular";
-import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { EffectsModule } from "@ngrx/effects";
+import { environment } from "../environments/environment";
 import { CoreModule } from "./core/core.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { UiModule } from "./modules/ui/ui.module";
@@ -14,24 +16,21 @@ import { UiModule } from "./modules/ui/ui.module";
 /* COMPONENTS */
 import { AppComponent } from "./app.component";
 
-/* REDUCERS */
-import { contactsReducer } from "./reducers/contacts.reducer";
-import { userReducer } from "./reducers/user.reducer";
-
-/* EFFECTS */
+/* NGRX */
+import { reducers } from "./app.state";
 import { ContactsEffects } from "./effects/contacts.effects";
 import { UserEffects } from "./effects/user.effects";
 
 /* OTHER */
 import { AuthInterceptor } from "./core/interceptors/CustomHttpInterceptor";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    StoreModule.forRoot({
-      contacts: contactsReducer,
-      user: userReducer
-    }),
+    StoreModule.forRoot(reducers),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot({ stateKey: "router" }),
     EffectsModule.forRoot([ContactsEffects /*UserEffects*/]),
     BrowserModule,
     HttpClientModule,
--- client/src/app/app.state.ts
+++ client/src/app/app.state.ts
@@ -1,6 +1,21 @@
+import { ActionReducerMap } from "@ngrx/store";
+import {
+  routerReducer,
+  RouterReducerState,
+  RouterStateSerializer
+} from "@ngrx/router-store";
+import { contactsReducer } from "./reducers/contacts.reducer";
+import { userReducer } from "./reducers/user.reducer";
 import { Contact } from "./core/models/Contact";
 
 export interface AppState {
+  readonly router: RouterReducerState<any>;
   readonly contacts: Contact[];
   readonly user: Contact | {};
 }
+
+export const reducers: ActionReducerMap<AppState> = {
+  router: routerReducer,
+  contacts: contactsReducer,
+  user: userReducer
+};
